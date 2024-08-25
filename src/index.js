abe0e3a59d8e0909e009f0b158cb7381a2f77065
gribVolnushka <burgerqueen98@gmail.com>
2024-08-25 15:34:21 +0200

    Fixed duplication of values and fixed names of existing variables

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-let a = prompt('Enter your name');
+const userName = prompt('Enter your name');
 
-alert(`Hi, ${a}`);
-
-let b = prompt('Enter your name');
-
-alert(`Hi, ${b}`);
-
-let c = prompt('Enter your name');
-
-alert(`Hi, ${c}`);
+alert(`Hi, ${userName}`);
